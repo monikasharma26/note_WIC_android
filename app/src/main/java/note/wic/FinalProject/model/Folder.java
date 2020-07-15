90a81038490840250472a15c5cdaaf66398b4fa1
monikasharma26 <monikasharma26081991@gmail.com>
2020-07-15 04:00:16 -0400

    [Monika] Folder Model Class Created

---
+++ app/src/main/java/note/wic/FinalProject/model/Folder.java
@@ -0,0 +1,88 @@
+package note.wic.FinalProject.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import java.util.Date;
+
+import note.wic.FinalProject.database.AppDatabase;
+
+@Table(database = AppDatabase.class, allFields = true)
+public class Folder extends BaseModel implements Parcelable{
+
+    @PrimaryKey(autoincrement = true)
+    private int id;
+    private String name;
+    private Date createdAt;
+
+    public Folder(){
+    }
+
+    protected Folder(Parcel in){
+        id = in.readInt();
+        name = in.readString();
+    }
+
+    public static final Parcelable.Creator<Folder> CREATOR = new Parcelable.Creator<Folder>(){
+        @Override
+        public Folder createFromParcel(Parcel in){
+            return new Folder(in);
+        }
+
+        @Override
+        public Folder[] newArray(int size){
+            return new Folder[size];
+        }
+    };
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Date getCreatedAt(){
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt){
+        this.createdAt = createdAt;
+    }
+
+    @Override public int describeContents(){
+        return 0;
+    }
+
+    @Override public void writeToParcel(Parcel dest, int flags){
+
+        dest.writeInt(id);
+        dest.writeString(name);
+    }
+
+    @Override public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Folder folder = (Folder) o;
+
+        return id == folder.id;
+    }
+
+    @Override public int hashCode(){
+        return id;
+    }
+}
