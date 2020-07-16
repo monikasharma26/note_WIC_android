8eca6fcb14e8893862d7504b2f9c9a1645fc0b30
monikasharma26 <monikasharma26081991@gmail.com>
2020-07-16 19:31:41 -0400

    [Monika] NoteListFragment added

--- app/src/main/java/note/wic/FinalProject/activites/note/NoteListFragment.java
+++ app/src/main/java/note/wic/FinalProject/activites/note/NoteListFragment.java
@@ -16,7 +16,7 @@ import note.wic.FinalProject.R;
  * create an instance of this fragment.
  */
 public class NoteListFragment extends Fragment {
-
+    public static final String FOLDER = "FOLDER";
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
