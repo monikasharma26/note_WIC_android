403545f4613d24e990dd3d5267350f9a44664836
monikasharma26 <monikasharma26081991@gmail.com>
2020-07-16 03:34:48 -0400

    [Monika] FoldersDAO updated

--- app/src/main/java/note/wic/FinalProject/database/FoldersDAO.java
+++ app/src/main/java/note/wic/FinalProject/database/FoldersDAO.java
@@ -1,4 +1,20 @@
 package note.wic.FinalProject.database;
 
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+import java.util.List;
+
+import note.wic.FinalProject.model.Folder;
+import note.wic.FinalProject.model.Folder_Table;
+
 public class FoldersDAO {
+
+    public static List<Folder> getLatestFolders(){
+        return SQLite.select().from(Folder.class).orderBy(Folder_Table.createdAt, false).queryList();
+    }
+
+    public static Folder getFolder(int id){
+        return SQLite.select().from(Folder.class).where(Folder_Table.id.is(id)).querySingle();
+    }
 }
+
