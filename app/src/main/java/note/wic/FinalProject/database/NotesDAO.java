88a5a515c6dba8b7eecafa4029cc86ed0fc5c6b8
monikasharma26 <monikasharma26081991@gmail.com>
2020-07-16 03:35:04 -0400

    [Monika] NotesDAO updated

--- app/src/main/java/note/wic/FinalProject/database/NotesDAO.java
+++ app/src/main/java/note/wic/FinalProject/database/NotesDAO.java
@@ -1,4 +1,23 @@
 package note.wic.FinalProject.database;
 
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+import java.util.List;
+
+import note.wic.FinalProject.model.Folder;
+import note.wic.FinalProject.model.Note;
+import note.wic.FinalProject.model.Note_Table;
+
 public class NotesDAO {
+
+    public static List<Note> getLatestNotes(Folder folder){
+        if (folder == null)
+            return SQLite.select().from(Note.class).orderBy(Note_Table.createdAt, false).queryList();
+        else
+            return FolderNoteDAO.getLatestNotes(folder);
+    }
+
+    public static Note getNote(int noteId){
+        return SQLite.select().from(Note.class).where(Note_Table.id.is(noteId)).querySingle();
+    }
 }
